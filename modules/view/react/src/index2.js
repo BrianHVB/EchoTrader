6bbad228c5620a428638e2da1bae6c31d5520610
Brian H <grimzecho@gmail.com>
2018-03-18 21:26:49 -0600

    modified webpack config to have multiple outputs
    Webpack will now look for both index.js and index2.js (to later be renamed) when bundeling JavaScript. The HTML plugin will also build two different HTML files based on each config.

---
+++ modules/view/react/src/index2.js
@@ -0,0 +1,7 @@
+alert("!");
+
+const msg = "Hello Multiverse 2";
+const doIt = () => console.log(msg);
+doIt();
+
+
--- modules/view/react/webpack.config.js
+++ modules/view/react/webpack.config.js
@@ -1,6 +1,15 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
+	entry: {
+		'main': './src/index.js',
+		'main2': `./src/index2.js`
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	},
 	module: {
 		rules: [
 			{
@@ -32,7 +41,13 @@ module.exports = {
 	plugins: [
 		new HtmlWebPackPlugin({
 			template: "./src/html/index.html",
-			filename: "./index.html"
+			filename: "./index.html",
+			chunks: [`main`]
+		}),
+		new HtmlWebPackPlugin({
+			template: "./src/html/index.html",
+			filename: "./index2.html",
+			chunks: [`main2`]
 		})
 	]
 };
